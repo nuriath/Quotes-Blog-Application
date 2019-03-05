150ddebba25d97fc5054e90ed4d0ef074db7d2f3
nuriath <mnuriath28@gmail.com>
2019-03-05 13:42:50 +0200

    edit files

--- app/__init__.py
+++ app/__init__.py
@@ -41,30 +41,3 @@ def create_app(config_name):
     return app
 
   
-
-# def create_app(config_name):
-
-#     app = Flask(__name__)
-
-#     # Creating the app configurations
-#     app.config.from_object(config_options[config_name])
-
-    
-    
-#     # Registering the blueprint
-#     from .main import main as main_blueprint
-#     app.register_blueprint(main_blueprint)
-
-#     from .request import configure_request
-#     configure_request(app)
-#     # Will add the views and forms
-
-#     from .auth import auth as auth_blueprint
-#     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
-
-#     # Initializing flask extensions
-#     bootstrap.init_app(app)
-#     db.init_app(app)
-#     # login_manager.init_app(app)
-
-#     return app
--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from .forms import PostForm,UpdateProfile,CommentForm,SubscribeForm
 from ..models import  User,Post,Comment,Subscribe
 from flask_login import login_required,current_user
 from .. import db,photos
-rom ..request import get_posts
+from ..request import get_posts
 
 
 # Pitch = pitch.Pitch
@@ -129,103 +129,3 @@ def delblog(id):
     title = 'delete blogs'
     return render_template('index.html',title = title, blog = blog)
 
-
-# @main.route('/pickup_line')
-# def pickup_line():
-  
-#     pickup_line_pitch = Pitch.query.filter_by(category='pickup_line').all()
-
-#     return render_template('index.html', pickup_line=pickup_line_pitch)
-
-# @main.route('/business')
-# def business():
-  
-#     business_pitch = Pitch.query.filter_by(category='business').all()
-
-#     return render_template('index.html', business=business_pitch)
-
-
-# @main.route('/jobs')
-# def jobs():
-#     jobs_pitch = Pitch.query.filter_by(category='jobs').all()
-#     return render_template('index.html', jobs=jobs_pitch)
-    
-# @main.route('/promotion')
-# def promotion():
-#     promotion_pitch = Pitch.query.filter_by(category='promotion').all()
-#     return render_template('index.html', promotion=promotion_pitch)
-    
-# @main.route('/user/<uname>/update',methods = ['GET','POST'])
-# @login_required
-# def update_profile(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if user is None:
-#         abort(404)
-
-#     form = UpdateProfile()
-
-#     if form.validate_on_submit():
-#         user.bio = form.bio.data
-
-#         db.session.add(user)
-#         db.session.commit()
-
-#         return redirect(url_for('.profile',uname=user.username))
-
-#     return render_template('profile/update.html',form =form)
-
-# @main.route('/user/<uname>/update/pic',methods= ['POST'])
-# @login_required
-# def update_pic(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if 'photo' in request.files:
-#         filename = photos.save(request.files['photo'])
-#         path = f'photos/{filename}'
-#         user.profile_pic_path = path
-#         db.session.commit()
-#     return redirect(url_for('main.profile',uname=uname))
-
-# @main.route('/user/<uname>')
-# def profile(uname):
-#     user = User.query.filter_by(username=uname).first()
-
-#     if user is None:
-#         abort(404)
-
-#     return render_template("profile/profile.html", user=user)
-
-# @main.route('/new', methods=['GET', 'POST'])
-# @login_required
-# def new_pitch():
-#     pitch_form = PitchForm()
-    
-#     if pitch_form.validate_on_submit():
-#         title = pitch_form.title.data
-#         content  = pitch_form.content.data
-#         username  = pitch_form.username.data
-#         category = pitch_form.category.data
-#         upvote = pitch_form.category.data
-#         user_id = pitch_form.user_id.data
-#         new_pitch = Pitch(title=title,content=content,category=category,user_id=current_user.id)
-#         new_pitch.save_pitch() 
-    
-#         return redirect(url_for('main.index'))
-
-#     return render_template('new_pitch.html', pitch_form=pitch_form)
-
-# @main.route('/comment/new/', methods=['GET', 'POST'])
-# @login_required
-# def comment(id):
-#     comment_form = CommentForm()
-    
-#     pitch = Pitch.query.get(id)
-
-#     if comment_form.validate_on_submit():
-#         comment = comment_form.comment.data
-#         category=category
-#         new_comment = Comment(comment=comment,user_id=user_id)
-#         new_comment.save_comment()
-#         return redirect(url_for('main.index'))
-
-#     return render_template('comment.html',comment_form=comment_form)
-    
