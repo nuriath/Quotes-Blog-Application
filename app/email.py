066d12e7c6ec061fa567bf69df28e04e5818b7ee
nuriath <mnuriath28@gmail.com>
2019-03-05 18:32:22 +0200

    edit email file

--- app/email.py
+++ app/email.py
@@ -7,7 +7,7 @@ def mail_message(subject,template,to,**kwargs):
    
     sender_password ="J0786154788"
 
-    # email = Message(subject, sender=sender_email, recipients=[to])
-    # email.body= render_template(template + ".txt",**kwargs)
-    # email.html = render_template(template + ".html",**kwargs)
-    # mail.send(email)
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body= render_template(template + ".txt",**kwargs)
+    email.html = render_template(template + ".html",**kwargs)
+    mail.send(email)
